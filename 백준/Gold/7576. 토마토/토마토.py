0f192a2c97afa87edb74998375eb775c9fc5d79b
DK3693 <ehdrb0169@naver.com>
2025-05-27 20:03:51 +0900

    [Gold V] Title: 토마토, Time: 2160 ms, Memory: 100444 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -8,13 +8,14 @@ queue = deque()
 for i in range(N):
         for j in range(M):
             if tomatoes[i][j] == 1:
-                queue.append((i, j))
+                queue.append((i, j, 0))
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-
+days = 0
 while queue:
-    y, x = queue.popleft()
+    y, x, day = queue.popleft()
+    days = day
 
     for dir in range(4):
         ny = y + dy[dir]
@@ -22,16 +23,10 @@ while queue:
 
         if 0 <= ny < N and 0 <= nx < M:
             if tomatoes[ny][nx] == 0:
-                tomatoes[ny][nx] = tomatoes[y][x] + 1
-                queue.append((ny, nx))
-
-    days = 0
-    
-if not any(0 in row for row in tomatoes): #모든 토마토가 익었는지 확인
-    for row in tomatoes:
-        days = max(days, max(row))
-    print(days - 1)
-    
-else:
+                tomatoes[ny][nx] = 1
+                queue.append((ny, nx, day + 1))
+  
+if any(0 in row for row in tomatoes): #모든 토마토가 익었는지 확인
     print(-1)
-
+else:
+    print(days)
