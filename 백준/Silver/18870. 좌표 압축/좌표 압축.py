718abc894fc88be0b8735beca7ab82375aa8938e
DK3693 <ehdrb0169@naver.com>
2025-08-08 19:29:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1688 ms, Memory: 149248 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,13 +1,8 @@
 import sys
 
-N = int(sys.stdin.readline().strip())
+_ = int(sys.stdin.readline())  # N은 사용 안 함
 nums = list(map(int, sys.stdin.readline().split()))
-nums_uniqued = list(set(nums))              # 중복값 제거
-nums_sorted = sorted(nums_uniqued)          # 정렬
 
+nums_dic = {v: i for i, v in enumerate(sorted(set(nums)))}
 
-nums_dic = {}
-for i in range(len(nums_sorted)):
-    nums_dic[nums_sorted[i]] = i            # 정렬된 상태로 딕셔너리에 저장
-
-print(*[nums_dic[n] for n in nums])           # 키값을 찾아 출력
+print(*[nums_dic[n] for n in nums])
