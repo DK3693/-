d983b31e38083aca1dda5dd3eaea1c5add5c31b1
DK3693 <ehdrb0169@naver.com>
2025-08-08 19:27:53 +0900

    [Silver II] Title: 좌표 압축, Time: 1804 ms, Memory: 149248 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -10,6 +10,4 @@ nums_dic = {}
 for i in range(len(nums_sorted)):
     nums_dic[nums_sorted[i]] = i            # 정렬된 상태로 딕셔너리에 저장
 
-for num in nums:
-    print(nums_dic[num], end=" ")           # 키값을 찾아 출력
-
+print(*[nums_dic[n] for n in nums])           # 키값을 찾아 출력
