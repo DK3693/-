398bf034ed6a4f074d2667c1b127eed88c3684f3
DK3693 <ehdrb0169@naver.com>
2025-08-18 14:43:19 +0900

    [Silver III] Title: 햄버거 분배, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,14 +1,21 @@
-N, K = map(int, input().split())
-burgers = list(input())
-count = 0
+import sys
+input = sys.stdin.readline
 
-for i in range(N):
-    if burgers[i] == 'P':
-        left = max(0, i - K)
-        right = min(N, i + K + 1)
-        for j in range(left, right):
-            if burgers[j] == 'H':
-                burgers[j] = 'A'
-                count += 1
-                break
-print(count)
+N, K = map(int, input().split())
+row = input().strip()
+
+H = [i for i, ch in enumerate(row) if ch == 'H']
+P = [i for i, ch in enumerate(row) if ch == 'P']
+
+i = j = ans = 0
+while i < len(P) and j < len(H):
+    if H[j] < P[i] - K:      # 이 햄버거는 너무 왼쪽 -> 다음 햄버거
+        j += 1
+    elif H[j] > P[i] + K:    # 이 사람에겐 너무 오른쪽 -> 다음 사람
+        i += 1
+    else:                    # |H[j] - P[i]| <= K → 매칭
+        ans += 1
+        i += 1
+        j += 1
+
+print(ans)
